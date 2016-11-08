1a3ca5478c8538226d536826b24b76fb7d2351bc
dovi.bergman@gmail.com <dovi.bergman@gmail.com>
2016-11-08 15:03:32 -0800

    Created Hike activity, added internet permission to manifest

--- app/src/main/java/watmok/tacoma/uw/edu/mylogin/HikeActivity.java
+++ app/src/main/java/watmok/tacoma/uw/edu/mylogin/HikeActivity.java
@@ -7,7 +7,9 @@ import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.View;
 
-public class HikeActivity extends AppCompatActivity {
+import watmok.tacoma.uw.edu.mylogin.hike.Hike;
+
+public class HikeActivity extends AppCompatActivity implements HikeFragment.OnListFragmentInteractionListener {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -16,14 +18,24 @@ public class HikeActivity extends AppCompatActivity {
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
 
-        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
+        if (savedInstanceState == null
+            && getSupportFragmentManager().findFragmentById(R.id.list) == null) {
+            HikeFragment hikeFragment = new HikeFragment();
+            getSupportFragmentManager().beginTransaction().add(R.id.fragment_container, hikeFragment).commit();
+        }
+
+        /*FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
         fab.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
                 Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
                         .setAction("Action", null).show();
             }
-        });
+        });*/
     }
 
+    @Override
+    public void onListFragmentInteraction(Hike item) {
+
+    }
 }
