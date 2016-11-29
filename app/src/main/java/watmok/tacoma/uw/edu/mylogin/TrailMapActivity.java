37454acd3e7be31fa7b23d05c581ea878bea47de
dovi.bergman@gmail.com <dovi.bergman@gmail.com>
2016-11-29 12:49:03 -0800

    changed php and added async task to map activity to display markes for trail

--- app/src/main/java/watmok/tacoma/uw/edu/mylogin/TrailMapActivity.java
+++ app/src/main/java/watmok/tacoma/uw/edu/mylogin/TrailMapActivity.java
@@ -1,11 +1,18 @@
 package watmok.tacoma.uw.edu.mylogin;
 
+import android.content.Context;
+import android.content.pm.PackageManager;
+import android.location.Criteria;
+import android.location.Location;
+import android.location.LocationManager;
 import android.os.AsyncTask;
+import android.support.v4.app.ActivityCompat;
 import android.support.v4.app.FragmentActivity;
 import android.os.Bundle;
 import android.util.Log;
 import android.widget.Toast;
 
+import com.google.android.gms.maps.CameraUpdate;
 import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
 import com.google.android.gms.maps.OnMapReadyCallback;
@@ -20,6 +27,7 @@ import java.net.HttpURLConnection;
 import java.net.URL;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.concurrent.TimeUnit;
 
 import watmok.tacoma.uw.edu.mylogin.hike.Hike;
 
@@ -42,10 +50,28 @@ public class TrailMapActivity extends FragmentActivity implements OnMapReadyCall
         DownloadHikesTask task = new DownloadHikesTask();
         task.execute(HIKES_URL);
 
+        waitForHikeTask();
+
         // Obtain the SupportMapFragment and get notified when the map is ready to be used.
         SupportMapFragment mapFragment = (SupportMapFragment) getSupportFragmentManager()
                 .findFragmentById(R.id.map);
         mapFragment.getMapAsync(this);
+    }
+
+    /**
+     * Waits up to 2 seconds for the DownloadHikesTask to load data into mHikeList;
+     */
+    private void waitForHikeTask() {
+        double counter = 0;
+        while (counter < 2 && mHikeList == null) {
+            try {
+                TimeUnit.SECONDS.sleep(1);
+                counter++;
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+
+        }
     }
 
 
@@ -61,6 +87,27 @@ public class TrailMapActivity extends FragmentActivity implements OnMapReadyCall
     public void onMapReady(GoogleMap googleMap) {
         mMap = googleMap;
 
+        if (ActivityCompat.checkSelfPermission(this, android.Manifest.permission.ACCESS_FINE_LOCATION)
+                != PackageManager.PERMISSION_GRANTED
+                && ActivityCompat.checkSelfPermission(this, android.Manifest.permission.ACCESS_COARSE_LOCATION)
+                != PackageManager.PERMISSION_GRANTED) {
+            return;
+        }
+        mMap.setMyLocationEnabled(true);
+        Location currentLocation = getMyLocation();
+
+        //check that location was found
+        if(currentLocation!=null) {
+            // recenter on current location and zoom in
+            LatLng position = new LatLng(currentLocation.getLatitude(),currentLocation.getLongitude());
+            Log.d("position:", "Position = " + position.toString());
+            mMap.moveCamera(CameraUpdateFactory.newLatLngZoom(position,9));
+
+
+        } else {
+            Log.e("position", "null position");
+        }
+
         // Add a marker for each hike
         if (mHikeList != null && !mHikeList.isEmpty()) {
             for (Hike hike : mHikeList) {
@@ -72,10 +119,42 @@ public class TrailMapActivity extends FragmentActivity implements OnMapReadyCall
             Log.e("null", "Unable to generate markers. mHikeList is null or empty");
         }
 
+
         // Add a marker in Sydney and move the camera
         /*LatLng sydney = new LatLng(-34, 151);
         mMap.addMarker(new MarkerOptions().position(sydney).title("Marker in Sydney"));
         mMap.moveCamera(CameraUpdateFactory.newLatLng(sydney));*/
+    }
+
+    /**
+     * @author This code is from http://stackoverflow.com/questions/14502102/zoom-on-current-user-location-displayed/14511032#14511032
+     * by DMCapps
+     * @return the best guess at user;s current location
+     */
+
+    private Location getMyLocation() {
+        if (ActivityCompat.checkSelfPermission(this, android.Manifest.permission.ACCESS_FINE_LOCATION)
+                != PackageManager.PERMISSION_GRANTED
+                && ActivityCompat.checkSelfPermission(this, android.Manifest.permission.ACCESS_COARSE_LOCATION)
+                != PackageManager.PERMISSION_GRANTED) {
+            return null;
+        }
+
+        // Get location from GPS if it's available
+        LocationManager lm = (LocationManager)getSystemService(Context.LOCATION_SERVICE);
+        Location myLocation = lm.getLastKnownLocation(LocationManager.GPS_PROVIDER);
+
+        // Location wasn't found, check the next most accurate place for the current location
+        if (myLocation == null) {
+            Criteria criteria = new Criteria();
+            criteria.setAccuracy(Criteria.ACCURACY_COARSE);
+            // Finds a provider that matches the criteria
+            String provider = lm.getBestProvider(criteria, true);
+            // Use the provider to get the last known location
+            myLocation = lm.getLastKnownLocation(provider);
+        }
+
+        return myLocation;
     }
 
     /**
