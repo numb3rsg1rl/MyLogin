ae60a7603bf9608c13efbf2b68b2de82a0069e58
dovi.bergman@gmail.com <dovi.bergman@gmail.com>
2016-12-02 14:04:30 -0800

    Made a lot progress with the hikeDetailPage, deleted unnecessary hike detail fragment

--- app/src/main/java/watmok/tacoma/uw/edu/mylogin/HikeDetailActivity.java
+++ app/src/main/java/watmok/tacoma/uw/edu/mylogin/HikeDetailActivity.java
@@ -1,11 +1,23 @@
 package watmok.tacoma.uw.edu.mylogin;
 
 import android.content.Intent;
+import android.os.AsyncTask;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.Toolbar;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import java.io.BufferedReader;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
+import java.net.URL;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.concurrent.TimeUnit;
 
 import watmok.tacoma.uw.edu.mylogin.hike.Hike;
 
@@ -13,36 +25,50 @@ public class HikeDetailActivity extends AppCompatActivity {
 
     private String myHikeName;
     private String lastActivity;
+    private Hike mHike;
+    private List<Hike> mhikeList;
+
+    private static final String HIKES_URL = "http://cssgate.insttech.washington.edu/~debergma/hikes.php?cmd=hike_detail";
+    private static final String HIKES_URL2 = "http://cssgate.insttech.washington.edu/~debergma/hikes.php?cmd=hike_image";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_hike_detail);
         Intent intent = getIntent();
-        if (intent.getStringExtra("PREVIOUS_ACTIVITY").equals("map")){
+        if (intent.getStringExtra("PREVIOUS_ACTIVITY").equals("map")) {
             setUpFromMap(intent.getStringExtra("TRAIL_NAME"));
         }
         //instantiate the Toolbar
         Toolbar myToolbar = (Toolbar) findViewById(R.id.my_toolbar);
         setSupportActionBar(myToolbar);
 
+        DownloadHikesTask task = new DownloadHikesTask();
+        task.execute(HIKES_URL);
+
+        waitForHikeTask();
+
+        DownloadPicturesTask task1 = new DownloadPicturesTask();
+        task1.execute(HIKES_URL2);
+
     }
 
     /**
      * Sets up the activity with parameters from the map
      */
-    protected void setUpFromMap (String hikeName){
+    protected void setUpFromMap(String hikeName) {
         lastActivity = "Map";
         myHikeName = hikeName;
     }
 
     /**
      * provides functionality for menu items
+     *
      * @param item the menu item that has been selected
      * @return always true
      */
     @Override
-    public boolean onOptionsItemSelected (MenuItem item) {
+    public boolean onOptionsItemSelected(MenuItem item) {
         if (item.getItemId() == R.id.back_item) {
             Intent i;
             if (lastActivity.equals("map")) {
@@ -64,16 +90,202 @@ public class HikeDetailActivity extends AppCompatActivity {
         return true;
     }
 
+    /**
+     * Waits up to 2 seconds for the DownloadHikesTask to load data into mHikeList;
+     */
+    private void waitForHikeTask() {
+        double counter = 0;
+        while (counter < 2 && mhikeList == null) {
+            try {
+                TimeUnit.SECONDS.sleep(1);
+                counter++;
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+
+        }
+    }
 
     /**
      * Inflates the menu Layout onto the toolbar
+     *
      * @param menu - the menu that needs a layout, in this case the Toolbar from onCreate()
      * @return returns true
      */
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
-        getMenuInflater().inflate(R.menu.menu,menu);
+        getMenuInflater().inflate(R.menu.menu, menu);
         return true;
     }
 
+    /**
+     * Helper method to create the text for the elevation imageView
+     *
+     * @param theElevation the elevation returned by the database
+     * @return A String that can go in the imageView
+     */
+    private String getMaxElevationText(String theElevation) {
+        if (theElevation != null) return "Max Elevation in Feet: " + theElevation;
+        return "Max Elevation in Feet: Not Available";
+    }
+
+    /**
+     * Helper method to create the text for the elevation gain imageView
+     *
+     * @param theElevationGain
+     * @return A String that can go in the imageView
+     */
+    private String getElevationGainText(String theElevationGain) {
+        if (theElevationGain != null) return "Elevation Gain in Feet: " + theElevationGain;
+        return "Elevation Gain in Feet: Not Available";
+    }
+
+    /**
+     * Helper method to create the text for the trail length imageView
+     *
+     * @param theLength the trail length from the mysql database
+     * @return A String that can go in the imageView
+     */
+    private String getTrailLengthText(String theLength) {
+        if (theLength != null) return "Trail Length in Miles: " + theLength;
+        return "Trail Length in Miles: Not Available";
+    }
+
+    /**
+     * A nested AsyncTask class that performs the actual business of connecting to the web service.
+     */
+    private class DownloadHikesTask extends AsyncTask<String, Void, String> {
+
+        /**
+         * Uses the URL(s) for the webservice to check for service and connect to the Hike database
+         *
+         * @param urls the url(s) for the web service
+         * @return a String with a JSON message, if successful, or an error message if something
+         * went wrong.
+         */
+        @Override
+        protected String doInBackground(String... urls) {
+
+            String response = "";
+            StringBuilder builder = new StringBuilder();
+            HttpURLConnection urlConnection = null;
+
+            for (String url : urls) {
+                try {
+
+                    URL urlObject = new URL(url);
+                    urlConnection = (HttpURLConnection) urlObject.openConnection();
+                    InputStream content = urlConnection.getInputStream();
+                    BufferedReader reader = new BufferedReader(new InputStreamReader(content));
+                    String s = "";
+                    while ((s = reader.readLine()) != null) {
+                        builder.append(s);
+                    }
+                    response = builder.toString();
+
+                } catch (Exception e) {
+                    response = "Unable to download the Hike list. Reason: " + e.getMessage();
+                }
+            }
+
+            return response;
+        }
+
+        /**
+         * Makes a toast if there was an error message to display it.
+         * Otherwise, calls the Hike class parseHikeJSON() method to fill the Hike list, and
+         * then passes that list with the HikeFragments mListener to the RecycleViewAdapter.
+         *
+         * @param result
+         */
+        protected void onPostExecute(String result) {
+            if (result.startsWith("Unable to")) {
+                Toast.makeText(getApplicationContext(),
+                        result, Toast.LENGTH_LONG).show();
+                return;
+            }
+            mhikeList = new ArrayList<>();
+            result = Hike.parseHikeJSON(result, mhikeList, true);
+            if (result != null) {
+                Toast.makeText(getApplicationContext(),
+                        result, Toast.LENGTH_LONG).show();
+                return;
+            }
+            mHike = mhikeList.get(0);
+
+
+        }
+
+
+
+    }
+    /**
+     * A nested AsyncTask class that performs the actual business of connecting to the web service.
+     */
+    private class DownloadPicturesTask extends AsyncTask<String, Void, String> {
+
+        /**
+         * Uses the URL(s) for the webservice to check for service and connect to the Hike database
+         *
+         * @param urls the url(s) for the web service
+         * @return a String with a JSON message, if successful, or an error message if something
+         * went wrong.
+         */
+        @Override
+        protected String doInBackground(String... urls) {
+
+            String response = "";
+            StringBuilder builder = new StringBuilder();
+            HttpURLConnection urlConnection = null;
+
+            for (String url : urls) {
+                try {
+
+                    URL urlObject = new URL(url);
+                    urlConnection = (HttpURLConnection) urlObject.openConnection();
+                    InputStream content = urlConnection.getInputStream();
+                    BufferedReader reader = new BufferedReader(new InputStreamReader(content));
+                    String s = "";
+                    while ((s = reader.readLine()) != null) {
+                        builder.append(s);
+                    }
+                    response = builder.toString();
+
+                } catch (Exception e) {
+                    response = "Unable to download the pictures list. Reason: " + e.getMessage();
+                }
+            }
+
+            return response;
+        }
+
+        /**
+         * Makes a toast if there was an error message to display it.
+         * Otherwise, calls the Hike class parseHikeJSON() method to fill the Hike list, and
+         * then passes that list with the HikeFragments mListener to the RecycleViewAdapter.
+         *
+         * @param result
+         */
+        protected void onPostExecute(String result) {
+            if (result.startsWith("Unable to")) {
+                Toast.makeText(getApplicationContext(),
+                        result, Toast.LENGTH_LONG).show();
+                return;
+            }
+            result = Hike.parseImageJSON(result, mhikeList);
+            if (result != null) {
+                Toast.makeText(getApplicationContext(),
+                        result, Toast.LENGTH_LONG).show();
+                return;
+            }
+            mHike = mhikeList.get(0);
+            displayHike();
+
+
+        }
+    }
+
+    private void displayHike() {
+                
+    }
 }
--- app/src/main/java/watmok/tacoma/uw/edu/mylogin/hike/Hike.java
+++ app/src/main/java/watmok/tacoma/uw/edu/mylogin/hike/Hike.java
@@ -1,5 +1,8 @@
 package watmok.tacoma.uw.edu.mylogin.hike;
 
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.util.Base64;
 import android.util.Log;
 
 import com.google.android.gms.maps.model.LatLng;
@@ -36,14 +39,28 @@ public class Hike implements Serializable {
      */
     private String mHikeName;
     private String mShortDescription;
+    private String mLongDescription;
     private LatLng mCoordinates;
+    private Bitmap mPicture;
+    private String mLength;
+    private String mMaxElevation;
+    private String mElevationGain;
+    private String mReviews;
+
 
     /**
      * Identifiers for parsing the JSON string
      */
-    public static final String HIKE_NAME = "Hike_Name";
-    public static final String SHORT_DESCRIPTION = "Short_Description";
-    public static final String TRAIL_COORDINATES = "Trailhead_Coordinates";
+    private static final String HIKE_NAME = "Hike_Name";
+    private static final String SHORT_DESCRIPTION = "Short_Description";
+    private static final String TRAIL_COORDINATES = "Trailhead_Coordinates";
+    private static final String PICTURE = "Picture";
+    private static final String LENGTH = "Length_In_Miles";
+    private static final String MAX_ELEV = "Max_Elevation_Ft";
+    private static final String ELEV_GAIN = "Elevation_Gain_Ft";
+    private static final String LONG_DESCRIPTION = "Long_Description";
+    private static final String REVIEWS ="Reviews";
+
 
     /**
      * Constructor for contrusting a hikes object with only the name and description.
@@ -80,6 +97,29 @@ public class Hike implements Serializable {
         mCoordinates = new LatLng(lat,lng);
     }
 
+
+    private Hike(String theHikeName, String theLongDescription, String theCoordinates,
+                 String theLength, String theElevGain, String theMaxElev, String theReviews) {
+        mHikeName = theHikeName;
+        mLength = theLength;
+        mLongDescription = theLongDescription;
+        mElevationGain = theElevGain;
+        mMaxElevation = theMaxElev;
+        mReviews = theReviews;
+        Scanner scanner = new Scanner(theCoordinates);
+
+        // the following parsing code is based on the first answer on
+        // http://stackoverflow.com/questions/26285086/reading-a-string-int-and-double-from-csv-file
+        String nextLine = scanner.nextLine();
+        Log.d("JSON of hike","String of the coordinates = " + theCoordinates);
+        String[] array = nextLine.split(",");
+        double lat = Double.parseDouble(array[0]);
+        double lng = Double.parseDouble(array[1]);
+
+        mCoordinates = new LatLng(lat,lng);
+
+    }
+
     /**
      * Parses a JSON String into a JSON array, and uses it to fill a List of Hike objects.
      * @author Daniel Bergman
@@ -99,12 +139,20 @@ public class Hike implements Serializable {
                     JSONObject object = array.getJSONObject(i);
                     Hike hike;
                     if (object.has(TRAIL_COORDINATES)) {
-                        hike = new Hike(object.getString(HIKE_NAME), object.getString(SHORT_DESCRIPTION),
+                        if (object.has(SHORT_DESCRIPTION)) {
+                            hike = new Hike(object.getString(HIKE_NAME), object.getString(SHORT_DESCRIPTION),
                                 object.getString(TRAIL_COORDINATES));
+                        } else {
+
+                            hike = new Hike(object.getString(HIKE_NAME), object.getString(LONG_DESCRIPTION),
+                                    object.getString(TRAIL_COORDINATES), object.getString(LENGTH),
+                                    object.getString(ELEV_GAIN), object.getString(MAX_ELEV),
+                                    object.getString(REVIEWS));
+                        }
                     } else {
                         hike = new Hike(object.getString(HIKE_NAME), object.getString(SHORT_DESCRIPTION));
                     }
-                    //TODO: If getUnsavesHikes is false, only add saved hikes from SQLite
+                    //TODO: If getUnsavedHikes is false, only add saved hikes from SQLite
 
                     hikeList.add(hike);
                 }
@@ -114,6 +162,81 @@ public class Hike implements Serializable {
         }
 
         return reason;
+    }
+
+    public static String parseImageJSON (String hikeJSON, List<Hike> hikeList) {
+        String reason = null;
+
+        if (hikeJSON != null) {
+            try {
+
+                JSONArray array = new JSONArray(hikeJSON);
+                for (int i = 0; i<array.length(); i++) {
+                    JSONObject object = array.getJSONObject(i);
+                    Hike hike = hikeList.get(i);
+                    String base64image = object.getString(PICTURE);
+                    byte[] rawImage = Base64.decode(base64image, Base64.DEFAULT);
+                    Bitmap bmp = BitmapFactory.decodeByteArray(rawImage, 0, rawImage.length);
+                    hike.setmPicture(bmp);
+                }
+            } catch (JSONException e) {
+                reason = "Unable to parse data. Reason: " + e.getMessage();
+            }
+        }
+
+        return reason;
+    }
+
+    public String getmLongDescription() {
+        return mLongDescription;
+    }
+
+    public void setmLongDescription(String mLongDescription) {
+        this.mLongDescription = mLongDescription;
+    }
+
+    public void setmCoordinates(LatLng mCoordinates) {
+        this.mCoordinates = mCoordinates;
+    }
+
+    public Bitmap getmPicture() {
+        return mPicture;
+    }
+
+    public void setmPicture(Bitmap mPicture) {
+        this.mPicture = mPicture;
+    }
+
+    public String getmLength() {
+        return mLength;
+    }
+
+    public void setmLength(String mLength) {
+        this.mLength = mLength;
+    }
+
+    public String getmMaxElevation() {
+        return mMaxElevation;
+    }
+
+    public void setmMaxElevation(String mMaxElevation) {
+        this.mMaxElevation = mMaxElevation;
+    }
+
+    public String getmElevationGain() {
+        return mElevationGain;
+    }
+
+    public void setmElevationGain(String mElevationGain) {
+        this.mElevationGain = mElevationGain;
+    }
+
+    public String getmReviews() {
+        return mReviews;
+    }
+
+    public void setmReviews(String mReviews) {
+        this.mReviews = mReviews;
     }
 
     /**
